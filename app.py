fb0f71b4bf28f4b147588b15837da0cb22ef9cc0
Juan Luque <juan.luque@alumni.esade.edu>
2018-12-23 19:06:58 +0100

    g
    g

--- app.py
+++ app.py
@@ -4,14 +4,11 @@
 # In[ ]:
 
 
-import json
-from textwrap import dedent as d
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
-from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 server = app.server
